0d3286a3af27b1e35ac7e7031bb81d21f301aed9
fema3832 <88377040+fema3832@users.noreply.github.com>
2022-01-07 22:43:48 +0100

    welcome fix

--- sources/javas.js
+++ sources/javas.js
@@ -12,5 +12,4 @@ function delay(delayInms) {
     });
 }
 
-    console.log("\n%c%cWelcome to fema.one \ud83d\udc96\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black",
-    "font-weight: bold; font-size: 50px; color: #ad6edd; text-shadow: 1px 1px 4px black")
+console.log("\n%c%cWelcome to fema.one \ud83d\udc96\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 50px; color: #ad6edd; text-shadow: 1px 1px 4px black")
