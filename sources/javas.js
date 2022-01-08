b40e880f9dfb3a57025df9111c809a2ab5eb0677
fema3832 <88377040+fema3832@users.noreply.github.com>
2022-01-08 14:03:45 +0100

    onload

--- sources/javas.js
+++ sources/javas.js
@@ -1,15 +1,7 @@
-document.addEventListener('DOMContentLoaded', (event) => {
+function pageload() {
     setTimeout(()=>{
         document.querySelector(".splash").classList.add("display-none");
     }, 2000);
-})
-
-function delay(delayInms) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-        resolve(2);
-        }, delayInms);
-    });
 }
 
 console.log("\n%c%cWelcome to fema.one \ud83d\udc96\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 50px; color: #ad6edd; text-shadow: 1px 1px 4px black")
