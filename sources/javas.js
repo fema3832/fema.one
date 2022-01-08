b1122391b3c70d06843aab08a3a6ff4e74528818
fema3832 <88377040+fema3832@users.noreply.github.com>
2022-01-08 14:06:38 +0100

    timeout fix

--- sources/javas.js
+++ sources/javas.js
@@ -1,7 +1,7 @@
 function pageload() {
     setTimeout(()=>{
         document.querySelector(".splash").classList.add("display-none");
-    }, 2000);
+    }, 1000);
 }
 
 console.log("\n%c%cWelcome to fema.one \ud83d\udc96\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 50px; color: #ad6edd; text-shadow: 1px 1px 4px black")
