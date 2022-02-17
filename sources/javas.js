192c25c04bcf25b95ffd47add1a9fca311724814
fema3832 <88377040+fema3832@users.noreply.github.com>
2022-02-17 17:46:10 +0100

    loader, pattern, line fix | added cursor

--- sources/javas.js
+++ sources/javas.js
@@ -1,7 +1,19 @@
-function pageload() {
-    setTimeout(()=>{
-        document.querySelector(".splash").classList.add("display-none");
-    }, 1000);
+window.onload = function () {
+    var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
+    document.getElementById("load").innerHTML = "load time: " + loadTime + " ms";
 }
 
-console.log("\n%c%cWelcome to fema.one \ud83d\udc96\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 50px; color: #ad6edd; text-shadow: 1px 1px 4px black")
+document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(()=>{
+        document.querySelector(".splash").classList.add("display-none");
+    }, 1000)
+})
+
+document.onkeydown = function (e) {
+    if(e.keyCode == 123) {
+        return false;
+    }
+}
+
+console.log("\n%c%cWelcome to fema.one \ud83d\udc96\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 45px; color: #ad6edd; text-shadow: 1px 1px 4px black")
+console.log("\n%c%cI purposely didn't disable ctrl+shift+i.\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black");
