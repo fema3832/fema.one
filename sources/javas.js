91a58403c6420e0ba1bbc677f2a4f365a49ac854
fema3832 <88377040+fema3832@users.noreply.github.com>
2022-02-18 13:01:33 +0100

    line, pattern responsive

--- sources/javas.js
+++ sources/javas.js
@@ -1,13 +1,7 @@
-window.onload = function () {
+window.onload = function() {
     var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
     document.getElementById("load").innerHTML = "load time: " + loadTime + " ms";
 }
-
-document.addEventListener("DOMContentLoaded", function(){
-    setTimeout(()=>{
-        document.querySelector(".splash").classList.add("display-none");
-    }, 1000)
-})
 
 document.onkeydown = function (e) {
     if(e.keyCode == 123) {
@@ -16,4 +10,3 @@ document.onkeydown = function (e) {
 }
 
 console.log("\n%c%cWelcome to fema.one \ud83d\udc96\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 45px; color: #ad6edd; text-shadow: 1px 1px 4px black")
-console.log("\n%c%cI purposely didn't disable ctrl+shift+i.\n","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black","font-weight: bold; font-size: 20px; color: #ad6edd; text-shadow: 1px 1px 4px black");
